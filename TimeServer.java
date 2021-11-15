5023bfbc34e9a7aaf7dabc454b467825378ed396
Thanos Mourtzoukos <thanosmour.tk@gmail.com>
2021-11-15 20:22:17 +0200

    Initial: commit

---
+++ TimeServer.java
@@ -0,0 +1,71 @@
+// The TimeServer for the time service...
+// Using UDP Datagrams and port number 8505
+// How to run directions together with TimeClient.java:
+// 1) Open a cmd window 
+// 2) Compile: javac TimeServer.java
+// 3) Execute: java TimeServer 
+// 4) Open a sepatare dos window (dont close TimeServer cmd window!!!!!) 
+// 5) Compile: javac TimeClient.java
+// 6)Execute: java TimeClient 127.0.0.1 
+import java.io.*;
+import java.net.*;
+import java.util.*; // For the date functions...
+public class TimeServer {
+    public static void main(String args[]) {
+        // Our datagram socket for I/O
+        DatagramSocket s = null;
+        // Our packet for getting the request and sending the 
+        // reply
+        DatagramPacket packet = null;
+        // Create a receive buffer
+        byte[] buf = new byte[256];
+        // Create the packet to receive the request
+        packet = new DatagramPacket(buf, buf.length);
+        // now create a socket to listen in
+        try {
+            s = new DatagramSocket(8505);
+        } catch(Exception e) {
+            System.out.println("Error : " + 
+            e.toString());
+        }
+        // Now sit in an infinite loop and reply to the 
+        // queries...
+        while (true) {
+            // sit around and wait for a request
+            try {
+                s.receive(packet);
+            } catch (Exception e) {
+                System.out.println("Error : " + 
+                e.toString());
+            }
+            // Get the client info
+            InetAddress cl = packet.getAddress();
+            int port = packet.getPort();
+            System.out.println("Client from " + cl.getHostAddress() 
+                + ":" + port + " requested the time.");
+            // Construct the response and sent it back...
+            //String localtime = new Date().toString();
+            Calendar rightNow = Calendar.getInstance();
+
+            String localtime =""+rightNow.get(Calendar.DATE)+"/"+(rightNow.get(Calendar.MONTH)+1)+"/"+ rightNow.get(Calendar.YEAR)+ "-"+rightNow.get(Calendar.HOUR )+":"+rightNow.get(Calendar.MINUTE)+":"+ rightNow.get(Calendar.SECOND);
+
+            //String localtime ="The Current Date is:" + rightNow.getTime()
+
+            // Convert the localtime string to an array 
+            // of bytes
+            localtime.getBytes(0, localtime.length(), buf, 0);
+            // Construct the packet
+            try {
+                packet = new DatagramPacket(buf,
+                buf.length, cl, port);
+                // Send it!
+                s.send(packet);
+            } catch (Exception e) {
+                System.out.println("Error : " + 
+                e.toString());
+            }
+            System.out.println("Client served...");
+        }
+    } // End of main()
+
+} // End of TimeServer class.
